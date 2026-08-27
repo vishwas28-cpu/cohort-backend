33aaa12d260e531b80b04b6334ba75d50c78cc52
vishwas28-cpu <vishwas.work28@gmail.com>
2026-08-27 09:27:05 +0530

    first practice

---
+++ day-1/app.js
@@ -0,0 +1,5 @@
+const express = require("express")
+
+const app = express()  //server create kiya 
+
+app.listen(3000)  // server start kiya
--- day-1/index.js
+++
@@ -1,3 +0,0 @@
-const Catme = require ("cat-me")
-
-console.log(Catme())
